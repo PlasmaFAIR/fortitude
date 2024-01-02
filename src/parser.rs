019742db3f8a2e73ad86127d40c77a2014458632
Liam Pattinson <liampattinson@gmail.com>
2024-01-02 17:12:14 +0000

    Move parser to new file

---
+++ src/parser.rs
@@ -0,0 +1,26 @@
+use tree_sitter::{Parser, Language};
+
+#[link(name = "tree-sitter-fortran")]
+extern "C" {
+    fn tree_sitter_fortran() -> Language;
+}
+
+fn fortran_parser() -> Parser {
+    let lang_fortran = unsafe{tree_sitter_fortran()};
+    let mut parser = Parser::new();
+    parser.set_language(lang_fortran).unwrap();
+    parser
+}
+
+
+#[test]
+fn test_fortran_parser() {
+    let mut parser = fortran_parser();
+    let source_code = "module test_mod\n\
+                       implicit none\n\
+                       character(len=*), parameter :: str = 'Hello World!'\n\
+                       end module test_mod";
+    let tree = parser.parse(source_code, None).unwrap();
+
+    println!("{}", tree.root_node().to_sexp());
+}
