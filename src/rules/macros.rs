cbf25f45daeb182fd7a0c2d036382561905d8c8f
Liam Pattinson <liampattinson@gmail.com>
2024-09-23 10:08:07 +0100

    Add AST_CODES, array of only AST-based rule codes
    Worked out at compile time using register_rules

--- src/rules/macros.rs
+++ src/rules/macros.rs
@@ -1,19 +1,46 @@
-macro_rules! num_rules {
-    ($rule: ident) => {
-        1
+macro_rules! num_ast {
+    (AST) => {1};
+    ($type: tt) => {0};
+    (AST, $($types: tt), +) => {
+        1 + num_ast!($($types), +)
     };
-    ($rule: path, $($rules: ident), +) => {
-        1 + num_rules!($($rules), +)
+    ($type: tt, $($types: tt), +) => {
+        num_ast!($($types), +)
+    }
+}
+
+macro_rules! option_ast {
+    (AST, $value: tt) => {
+        Some($value)
+    };
+    ($type: tt, $value: tt) => {
+        None
     };
 }
 
 #[macro_export]
 macro_rules! register_rules {
-    ($(($categories: ty, $codes: literal, $paths: path, $rules: ident)), +) => {
+    ($(($categories: ty, $codes: literal, $types: tt, $paths: path, $rules: ident)), +) => {
 
-        const N_RULES: usize = num_rules!($($rules),+);
+        const CODES: &[&str; [$($codes), *].len()] = &[$($codes), +];
 
-        const CODES: &[&str; N_RULES] = &[$($codes,) +];
+        const fn drop_none<'a, const N1: usize, const N2: usize>(xs: &'a [Option<&'a str>; N1]) -> [&'a str; N2] {
+            let mut result: [&str; N2] = [""; N2];
+            let mut i = 0;
+            let mut j = 0;
+            while i < N1 {
+                if let Some(y) = xs[i] {
+                    result[j] = y;
+                    j += 1;
+                }
+                i += 1;
+            }
+            result
+        }
+
+        const N_AST: usize = num_ast!($($types), +);
+        const AST_OPTIONS: &[Option<&str>; CODES.len()] = &[$(option_ast!($types, $codes)), +];
+        const AST_CODES: &[&str; N_AST] = &drop_none(AST_OPTIONS);
 
         $(type $rules = $paths;)+
 
--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -12,20 +12,20 @@ use crate::Rule;
 use std::collections::{BTreeMap, BTreeSet};
 
 register_rules! {
-    (Category::Error, "E001", error::syntax_error::SyntaxError, SyntaxError),
-    (Category::Filesystem, "F001", filesystem::extensions::NonStandardFileExtension, NonStandardFileExtension),
-    (Category::Style, "S001", style::line_length::LineTooLong, LineTooLong),
-    (Category::Style, "S101", style::whitespace::TrailingWhitespace, TrailingWhitespace),
-    (Category::Typing, "T001", typing::implicit_typing::ImplicitTyping, ImplicitTyping),
-    (Category::Typing, "T002", typing::implicit_typing::InterfaceImplicitTyping, InterfaceImplicitTyping),
-    (Category::Typing, "T003", typing::implicit_typing::SuperfluousImplicitNone, SuperfluousImplicitNone),
-    (Category::Typing, "T011", typing::literal_kinds::LiteralKind, LiteralKind),
-    (Category::Typing, "T012", typing::literal_kinds::LiteralKindSuffix, LiteralKindSuffix),
-    (Category::Typing, "T021", typing::star_kinds::StarKind, StarKind),
-    (Category::Precision, "P001", precision::kind_suffixes::NoRealSuffix, NoRealSuffix),
-    (Category::Precision, "P011", precision::double_precision::DoublePrecision, DoublePrecision),
-    (Category::Modules, "M001", modules::external_functions::ExternalFunction, ExternalFunction),
-    (Category::Modules, "M011", modules::use_statements::UseAll, UseAll)
+    (Category::Error, "E001", AST, error::syntax_error::SyntaxError, SyntaxError),
+    (Category::Filesystem, "F001", PATH, filesystem::extensions::NonStandardFileExtension, NonStandardFileExtension),
+    (Category::Style, "S001", TEXT, style::line_length::LineTooLong, LineTooLong),
+    (Category::Style, "S101", TEXT, style::whitespace::TrailingWhitespace, TrailingWhitespace),
+    (Category::Typing, "T001", AST, typing::implicit_typing::ImplicitTyping, ImplicitTyping),
+    (Category::Typing, "T002", AST, typing::implicit_typing::InterfaceImplicitTyping, InterfaceImplicitTyping),
+    (Category::Typing, "T003", AST, typing::implicit_typing::SuperfluousImplicitNone, SuperfluousImplicitNone),
+    (Category::Typing, "T011", AST, typing::literal_kinds::LiteralKind, LiteralKind),
+    (Category::Typing, "T012", AST, typing::literal_kinds::LiteralKindSuffix, LiteralKindSuffix),
+    (Category::Typing, "T021", AST, typing::star_kinds::StarKind, StarKind),
+    (Category::Precision, "P001", AST, precision::kind_suffixes::NoRealSuffix, NoRealSuffix),
+    (Category::Precision, "P011", AST, precision::double_precision::DoublePrecision, DoublePrecision),
+    (Category::Modules, "M001", AST, modules::external_functions::ExternalFunction, ExternalFunction),
+    (Category::Modules, "M011", AST, modules::use_statements::UseAll, UseAll)
 }
 
 pub type RuleBox = Box<dyn Rule>;
