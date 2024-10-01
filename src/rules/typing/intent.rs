72d99dd36d9670a74e51cc9117068c4315bafc08
Peter Hill <peter.hill@york.ac.uk>
2024-10-01 14:28:11 +0100

    Try to improve missing intent explanation

--- src/rules/typing/intent.rs
+++ src/rules/typing/intent.rs
@@ -14,8 +14,20 @@ impl Rule for MissingIntent {
 
     fn explain(&self) -> &'static str {
         "
-        Dummy arguments should have an explicit `intent` attribute. This can help
-        catch logic errors, and potentially improve performance
+        Procedure dummy arguments should have an explicit `intent`
+        attributes. This can help catch logic errors, potentially improve
+        performance, as well as serving as documentation for users of
+        the procedure.
+
+        Arguments with `intent(in)` are read-only input variables, and cannot be
+        modified by the routine.
+
+        Arguments with `intent(out)` are output variables, and their value on
+        entry into the routine can be safely ignored.
+
+        Finally, `intent(inout)` arguments can be both read and modified by the
+        routine. If an `intent` is not specified, it will default to
+        `intent(inout)`.
         "
     }
 }
