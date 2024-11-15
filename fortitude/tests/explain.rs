336e7e7f609a29ebf077ba74dafc336119f32748
Peter Hill <peter.hill@york.ac.uk>
2024-11-15 10:34:18 +0000

    Reduce duplication in `explain` integrated tests

--- fortitude/tests/explain.rs
+++ fortitude/tests/explain.rs
@@ -2,12 +2,13 @@ use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
 
+const BIN_NAME: &str = "fortitude";
+
 #[test]
 fn explain_all() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin("fortitude")?;
-
-    cmd.arg("explain");
-    cmd.assert()
+    Command::cargo_bin(BIN_NAME)?
+        .arg("explain")
+        .assert()
         .success()
         .stdout(predicate::str::contains("T001"))
         .stdout(predicate::str::contains("S061"));
@@ -17,10 +18,10 @@ fn explain_all() -> anyhow::Result<()> {
 
 #[test]
 fn explain_nonexistent_rule() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin("fortitude")?;
-
-    cmd.arg("explain").arg("X99999");
-    cmd.assert()
+    Command::cargo_bin(BIN_NAME)?
+        .arg("explain")
+        .arg("X99999")
+        .assert()
         .failure()
         .stderr(predicate::str::contains("invalid"));
 
@@ -29,10 +30,10 @@ fn explain_nonexistent_rule() -> anyhow::Result<()> {
 
 #[test]
 fn explain_one_rule() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin("fortitude")?;
-
-    cmd.arg("explain").arg("T001");
-    cmd.assert()
+    Command::cargo_bin(BIN_NAME)?
+        .arg("explain")
+        .arg("T001")
+        .assert()
         .success()
         .stdout(predicate::str::contains("T001"))
         .stdout(predicate::str::contains("S061").count(0));
@@ -42,10 +43,10 @@ fn explain_one_rule() -> anyhow::Result<()> {
 
 #[test]
 fn explain_one_rule_by_name() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin("fortitude")?;
-
-    cmd.arg("explain").arg("implicit-typing");
-    cmd.assert()
+    Command::cargo_bin(BIN_NAME)?
+        .arg("explain")
+        .arg("implicit-typing")
+        .assert()
         .success()
         .stdout(predicate::str::contains("T001"))
         .stdout(predicate::str::contains("S061").count(0));
@@ -55,10 +56,10 @@ fn explain_one_rule_by_name() -> anyhow::Result<()> {
 
 #[test]
 fn explain_category() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin("fortitude")?;
-
-    cmd.arg("explain").arg("T");
-    cmd.assert()
+    Command::cargo_bin(BIN_NAME)?
+        .arg("explain")
+        .arg("T")
+        .assert()
         .success()
         .stdout(predicate::str::contains("T001"))
         .stdout(predicate::str::contains("T002"))
@@ -69,10 +70,10 @@ fn explain_category() -> anyhow::Result<()> {
 
 #[test]
 fn explain_category_by_name() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin("fortitude")?;
-
-    cmd.arg("explain").arg("typing");
-    cmd.assert()
+    Command::cargo_bin(BIN_NAME)?
+        .arg("explain")
+        .arg("typing")
+        .assert()
         .success()
         .stdout(predicate::str::contains("T001"))
         .stdout(predicate::str::contains("T002"))
@@ -83,10 +84,12 @@ fn explain_category_by_name() -> anyhow::Result<()> {
 
 #[test]
 fn explain_mixed_multiple() -> anyhow::Result<()> {
-    let mut cmd = Command::cargo_bin("fortitude")?;
-
-    cmd.arg("explain").arg("T").arg("use-all").arg("P021");
-    cmd.assert()
+    Command::cargo_bin(BIN_NAME)?
+        .arg("explain")
+        .arg("T")
+        .arg("use-all")
+        .arg("P021")
+        .assert()
         .success()
         .stdout(predicate::str::contains("T001"))
         .stdout(predicate::str::contains("T002"))
@@ -96,4 +99,3 @@ fn explain_mixed_multiple() -> anyhow::Result<()> {
 
     Ok(())
 }
-
