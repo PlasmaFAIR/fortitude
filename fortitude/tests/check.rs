2c2b9578ef876dd4875245bb9b043e49f261c7fa
Peter Hill <peter.hill@york.ac.uk>
2024-11-15 12:12:24 +0000

    Add some more integrated tests for `check`

--- fortitude/tests/check.rs
+++ fortitude/tests/check.rs
@@ -1,7 +1,7 @@
 use assert_cmd::prelude::*;
 use insta_cmd::assert_cmd_snapshot;
-use tempfile::TempDir;
 use std::{fs, process::Command};
+use tempfile::TempDir;
 
 const BIN_NAME: &str = "fortitude";
 
@@ -44,12 +44,13 @@ fn check_file_doesnt_exist() -> anyhow::Result<()> {
     Ok(())
 }
 
-
 #[test]
-fn check_select() -> anyhow::Result<()> {
+fn check_all() -> anyhow::Result<()> {
     let tempdir = TempDir::new()?;
     let test_file = tempdir.path().join("test.f90");
-    fs::write(&test_file, r#"
+    fs::write(
+        &test_file,
+        r#"
 program test
   logical*4, parameter :: true = .true.
 end program
@@ -99,6 +100,243 @@ end program
 
     fortitude: 1 files scanned.
     Number of errors: 4
+
+    For more information about specific rules, run:
+
+        fortitude explain X001,Y002,...
+
+
+    ----- stderr -----
+    ");
+    Ok(())
+}
+
+#[test]
+fn check_select_cli() -> anyhow::Result<()> {
+    let tempdir = TempDir::new()?;
+    let test_file = tempdir.path().join("test.f90");
+    fs::write(
+        &test_file,
+        r#"
+program test
+  logical*4, parameter :: true = .true.
+end program
+"#,
+    )?;
+
+    apply_common_filters!();
+    assert_cmd_snapshot!(Command::cargo_bin(BIN_NAME)?
+                         .arg("check")
+                         .arg(test_file)
+                         .arg("--select=T001,style"),
+                         @r"
+    success: false
+    exit_code: 1
+    ----- stdout -----
+    warning: [TEMP_FILE] T001 program missing 'implicit none'
+      |
+    2 | program test
+      | ^^^^^^^^^^^^ T001
+    3 |   logical*4, parameter :: true = .true.
+    4 | end program
+      |
+
+    warning: [TEMP_FILE] S061 end statement should read 'end program test'
+      |
+    2 | program test
+    3 |   logical*4, parameter :: true = .true.
+    4 | end program
+      | ^^^^^^^^^^^ S061
+      |
+
+
+    fortitude: 1 files scanned.
+    Number of errors: 2
+
+    For more information about specific rules, run:
+
+        fortitude explain X001,Y002,...
+
+
+    ----- stderr -----
+    ");
+    Ok(())
+}
+
+#[test]
+fn check_select_file() -> anyhow::Result<()> {
+    let tempdir = TempDir::new()?;
+    let test_file = tempdir.path().join("test.f90");
+    fs::write(
+        &test_file,
+        r#"
+program test
+  logical*4, parameter :: true = .true.
+end program
+"#,
+    )?;
+
+    let config_file = tempdir.path().join("fortitude.toml");
+    fs::write(
+        &config_file,
+        r#"
+[check]
+select = ["T001", "style"]
+"#,
+    )?;
+
+    apply_common_filters!();
+    assert_cmd_snapshot!(Command::cargo_bin(BIN_NAME)?
+                         .args(["--config-file", config_file.as_os_str().to_string_lossy().as_ref()])
+                         .arg("check")
+                         .arg(test_file),
+                         @r"
+    success: false
+    exit_code: 1
+    ----- stdout -----
+    warning: [TEMP_FILE] T001 program missing 'implicit none'
+      |
+    2 | program test
+      | ^^^^^^^^^^^^ T001
+    3 |   logical*4, parameter :: true = .true.
+    4 | end program
+      |
+
+    warning: [TEMP_FILE] S061 end statement should read 'end program test'
+      |
+    2 | program test
+    3 |   logical*4, parameter :: true = .true.
+    4 | end program
+      | ^^^^^^^^^^^ S061
+      |
+
+
+    fortitude: 1 files scanned.
+    Number of errors: 2
+
+    For more information about specific rules, run:
+
+        fortitude explain X001,Y002,...
+
+
+    ----- stderr -----
+    ");
+    Ok(())
+}
+
+#[test]
+fn check_extend_select_file() -> anyhow::Result<()> {
+    let tempdir = TempDir::new()?;
+    let test_file = tempdir.path().join("test.f90");
+    fs::write(
+        &test_file,
+        r#"
+program test
+  logical*4, parameter :: true = .true.
+end program
+"#,
+    )?;
+
+    let config_file = tempdir.path().join("fortitude.toml");
+    fs::write(
+        &config_file,
+        r#"
+[check]
+select = ["T001"]
+"#,
+    )?;
+
+    apply_common_filters!();
+    assert_cmd_snapshot!(Command::cargo_bin(BIN_NAME)?
+                         .args(["--config-file", config_file.as_os_str().to_string_lossy().as_ref()])
+                         .arg("check")
+                         .arg(test_file)
+                         .arg("--extend-select")
+                         .arg("style"),
+                         @r"
+    success: false
+    exit_code: 1
+    ----- stdout -----
+    warning: [TEMP_FILE] T001 program missing 'implicit none'
+      |
+    2 | program test
+      | ^^^^^^^^^^^^ T001
+    3 |   logical*4, parameter :: true = .true.
+    4 | end program
+      |
+
+    warning: [TEMP_FILE] S061 end statement should read 'end program test'
+      |
+    2 | program test
+    3 |   logical*4, parameter :: true = .true.
+    4 | end program
+      | ^^^^^^^^^^^ S061
+      |
+
+
+    fortitude: 1 files scanned.
+    Number of errors: 2
+
+    For more information about specific rules, run:
+
+        fortitude explain X001,Y002,...
+
+
+    ----- stderr -----
+    ");
+    Ok(())
+}
+
+#[test]
+fn check_select_file_fpm_toml() -> anyhow::Result<()> {
+    let tempdir = TempDir::new()?;
+    let test_file = tempdir.path().join("test.f90");
+    fs::write(
+        &test_file,
+        r#"
+program test
+  logical*4, parameter :: true = .true.
+end program
+"#,
+    )?;
+
+    let config_file = tempdir.path().join("fpm.toml");
+    fs::write(
+        &config_file,
+        r#"
+[extra.fortitude.check]
+select = ["T001", "style"]
+"#,
+    )?;
+
+    apply_common_filters!();
+    assert_cmd_snapshot!(Command::cargo_bin(BIN_NAME)?
+                         .args(["--config-file", config_file.as_os_str().to_string_lossy().as_ref()])
+                         .arg("check")
+                         .arg(test_file),
+                         @r"
+    success: false
+    exit_code: 1
+    ----- stdout -----
+    warning: [TEMP_FILE] T001 program missing 'implicit none'
+      |
+    2 | program test
+      | ^^^^^^^^^^^^ T001
+    3 |   logical*4, parameter :: true = .true.
+    4 | end program
+      |
+
+    warning: [TEMP_FILE] S061 end statement should read 'end program test'
+      |
+    2 | program test
+    3 |   logical*4, parameter :: true = .true.
+    4 | end program
+      | ^^^^^^^^^^^ S061
+      |
+
+
+    fortitude: 1 files scanned.
+    Number of errors: 2
 
     For more information about specific rules, run:
 
