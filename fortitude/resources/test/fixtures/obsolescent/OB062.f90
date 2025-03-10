9dca582881081557b105007244b391f13d919a4a
Liam Pattinson <liampattinson@gmail.com>
2025-03-10 10:15:05 +0000

    Expand DeprecatedCharacterSyntax test

--- fortitude/resources/test/fixtures/obsolescent/OB062.f90
+++ fortitude/resources/test/fixtures/obsolescent/OB062.f90
@@ -4,9 +4,10 @@ program p
 
   character*5 :: hello
   character(len=5) :: world
-  character*   5 :: foo
+  character*   5 foo
   CHARACTER   *5 :: bar
   charaCTeR   *  5 :: baz
+  character*10 x, y, z
 
   hello = "hello"
   world = "world"
