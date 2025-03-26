eb491cde94d93d9c604c0229fbe1d3af44db9d15
Liam Pattinson <liampattinson@gmail.com>
2025-03-26 17:17:02 +0000

    Docs fix for double-precision-literal

--- fortitude/src/rules/modernisation/double_precision.rs
+++ fortitude/src/rules/modernisation/double_precision.rs
@@ -80,10 +80,10 @@ impl AstRule for DoublePrecision {
 /// Checks for floating point literals that using `d` or `D` as the exponentiation.
 ///
 /// ## Why is this bad?
-/// Floating point literals using `d` or `D` as the exponentiation, such as `1.23d2`,
-/// will be of the `double precision` kind. This is commonly assumed to be a 64-bit
-/// float, but is not guaranteed to be so, and may vary depending on your system and
-/// compiler arguments.
+/// Floating point literals using `d` or `D` for the exponent, such as `1.23d2`,
+/// will be of the `double precision` kind. This is commonly assumed to be a
+/// 64-bit float, but is not guaranteed to be so, and may vary depending on your
+/// system and compiler arguments.
 ///
 /// In modern Fortran, it is preferred to set the required precision using
 /// 'kinds'. For portability, it is recommended to use `real(dp)`, with `dp` set
@@ -93,10 +93,11 @@ impl AstRule for DoublePrecision {
 /// - `integer, parameter :: dp = selected_real_kind(15, 307)`
 ///
 /// For code that should be compatible with C, you should instead use
-/// `real(c_double)`, which may be found in the intrinsic module `iso_c_binding`.
-/// To ensure floating point literals match the kind of the variable they are assigned
-/// to, it is recommended to use `e` or `E` for exponentiation and a kind suffix, so
-/// `1.23d2` should be written as `1.23e2_dp`.
+/// `real(c_double)`, which may be found in the intrinsic module
+/// `iso_c_binding`.  To ensure floating point literals match the kind of the
+/// variable they are assigned to, it is recommended to use `e` or `E` for
+/// exponentiation and a kind suffix, so `1.23d2` should be written as
+/// `1.23e2_dp`.
 ///
 /// ## References
 /// - Metcalf, M., Reid, J. and Cohen, M., 2018, _Modern Fortran Explained: Incorporating Fortran
