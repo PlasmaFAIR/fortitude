86f957a5a55b4784c1281f9663f0f4013516f357
Peter Hill <peter.hill@york.ac.uk>
2025-05-16 10:16:43 +0100

    Fix escaped quote rules for empty literals

--- fortitude/resources/test/fixtures/style/S242.f90
+++ fortitude/resources/test/fixtures/style/S242.f90
@@ -1,4 +1,5 @@
 program test
+  use, intrinsic :: iso_c_binding, only: c_char
   implicit none (type, external)
 
   print*, 'This isn''t necessary'
@@ -6,4 +7,11 @@ program test
   print*, 'This "isn''t" unnecessary'
   print*, "This ""isn't"" unnecessary"
   print*, 4_"Does this ""break""?"
+  print*, c_char_"Does this ''break''?"
+  print*, 'This &
+       &isn''t &
+       &necessary'
+  print*, 'This &
+       &""isn''t"" &
+       &unnecessary'
 end program test
