de7ea089e463c132290fc691573142a7f2d05245
Liam Pattinson <liampattinson@gmail.com>
2025-12-04 11:27:57 +0000

    Implement multiline c comments in LogicalLines
    Multiline comments are handled by retaining only the
    contents in the last line up to the termination.

--- crates/fortitude_preprocessor/src/logical_lines.rs
+++ crates/fortitude_preprocessor/src/logical_lines.rs
@@ -1,3 +1,4 @@
+use crate::tokens::{CppTokenIterator, CppTokenKind};
 use ruff_source_file::{OneIndexed, SourceCode};
 use ruff_text_size::TextSize;
 use std::borrow::Cow;
@@ -47,34 +48,39 @@ impl<'a> LogicalLine<'a> {
         )
     }
 
-    /// Merge two logical lines at the given end and start indices, returning
+    /// Merge two logical lines at the end index of `self`, returning
     /// a new LogicalLine.
-    pub fn merge(
-        self,
-        right: LogicalLine,
-        left_end: TextSize,
-        right_start: TextSize,
-    ) -> LogicalLine<'a> {
-        let merged_text = {
-            let left_part = &self.text[..left_end.to_usize()];
-            let right_part = &right.text[right_start.to_usize()..];
-            left_part.to_owned() + right_part
-        };
+    fn merge(self, other: LogicalLine, end_index: TextSize) -> LogicalLine<'a> {
+        let merged_text = self.text()[..end_index.to_usize()].to_owned() + other.text();
         let merged_byte_map = {
             let mut map = self.byte_map.clone();
             // Remove any entries in left.byte_map after left_end
-            map.retain(|&k, _| k < left_end);
-            // Add beginning entry for right_start
-            map.insert(left_end, right.offset(right_start));
-            // Add entries from right.byte_map after right_start, adjusted for offset
-            for (src, dest) in right.byte_map.iter().filter(|(k, _)| *k > &right_start) {
-                map.insert(left_end + src - right_start, *dest);
+            map.retain(|&k, _| k < end_index);
+            // Add entries from right.byte_map
+            for (src, dest) in other.byte_map {
+                map.insert(src + end_index, dest);
             }
             map
         };
         LogicalLine {
             text: Cow::Owned(merged_text),
             byte_map: merged_byte_map,
+        }
+    }
+
+    /// Determine local byte offset of the start of an unterminated comment.
+    /// The byte returned is the first byte within the comment itself.
+    /// Returns None if no unterminated comment is found.
+    fn unterminated_comment_start(&self) -> Option<TextSize> {
+        // Tokenise the line to avoid comment starts inside string literals.
+        // While the last token should ordinarily be a newline, if the line
+        // ends with an unterminated comment, the newline will be contained
+        // within the comment token.
+        let last_token = CppTokenIterator::new(self.text()).last()?;
+        if last_token.kind == CppTokenKind::Comment {
+            Some(last_token.start + TextSize::from(2)) // +2 to get past the /*
+        } else {
+            None
         }
     }
 }
@@ -84,6 +90,9 @@ pub struct LogicalLines<'a> {
 }
 
 impl<'a> LogicalLines<'a> {
+    /// Generate unmerged logical lines from source code.
+    /// Each line corresponds directly to a physical line in the source code,
+    /// and borrows the text from the source code.
     fn from_source_code_unmerged(src: &'a SourceCode) -> LogicalLines<'a> {
         let mut lines = Vec::new();
         let line_count = src.line_count();
@@ -96,6 +105,8 @@ impl<'a> LogicalLines<'a> {
         LogicalLines { lines }
     }
 
+    /// Iterate over all logical lines, merging those that are continued
+    /// with a trailing backslash.
     fn merge_escaped_newlines(self) -> Self {
         let mut new = Vec::new();
         let mut prev: Option<LogicalLine<'_>> = None;
@@ -106,9 +117,8 @@ impl<'a> LogicalLines<'a> {
                     if let Some(continued) = trimmed.strip_suffix('\\') {
                         // If the previous line ends with a backslash, merge it
                         // with this line
-                        let left_end = TextSize::from(continued.len() as u32);
-                        let right_start = TextSize::from(0);
-                        let merged = prev_line.merge(line, left_end, right_start);
+                        let end_index = TextSize::from(continued.len() as u32);
+                        let merged = prev_line.merge(line, end_index);
                         prev = Some(merged);
                     } else {
                         // If not, insert the previous line and set this line as
@@ -129,9 +139,51 @@ impl<'a> LogicalLines<'a> {
         LogicalLines { lines: new }
     }
 
+    /// Iterate over all logical lines, merging those linked by a multi-line
+    /// C-style comment. The comment in the merged line will contain only the
+    /// contents on the last line up to the terminator.
+    fn merge_multiline_comments(self) -> Self {
+        let mut new = Vec::new();
+        let mut unterminated: Option<(LogicalLine<'_>, TextSize)> = None;
+        for line in self.lines.into_iter() {
+            match unterminated {
+                Some((prev_line, end_index)) => {
+                    // If we have an unterminated comment, merge it with this line
+                    let merged = prev_line.merge(line, end_index);
+                    // If the merged line still has an unterminated comment,
+                    // store it. Otherwise, insert the merged line and clear the
+                    // unterminated state.
+                    let new_idx = merged.unterminated_comment_start();
+                    if let Some(new_idx) = new_idx {
+                        unterminated = Some((merged, new_idx));
+                    } else {
+                        new.push(merged);
+                        unterminated = None;
+                    }
+                }
+                None => {
+                    let idx = line.unterminated_comment_start();
+                    if let Some(idx) = idx {
+                        // If the line contains an unterminated comment, store it
+                        unterminated = Some((line, idx));
+                    } else {
+                        // Otherwise, insert the line directly
+                        new.push(line);
+                    }
+                }
+            }
+        }
+        // Insert the last line if it exists
+        if let Some((last_line, _)) = unterminated {
+            new.push(last_line);
+        }
+        LogicalLines { lines: new }
+    }
+
     pub fn from_source_code(src: &'a SourceCode) -> LogicalLines<'a> {
-        // TODO implement merging of multiline comments
-        LogicalLines::from_source_code_unmerged(src).merge_escaped_newlines()
+        LogicalLines::from_source_code_unmerged(src)
+            .merge_escaped_newlines()
+            .merge_multiline_comments()
     }
 }
 
@@ -173,6 +225,81 @@ mod tests {
         assert_eq!(lines.len(), 3);
         assert_eq!(lines[0].text(), "program p\n");
         assert_eq!(lines[1].text(), "  implicit none\n");
+        assert_eq!(lines[2].text(), "end program p");
+    }
+
+    #[test]
+    fn test_logical_lines_multiline_comments() {
+        let code = dedent!(
+            r#"
+            program p
+              print *, /**/ "/* hello /*" /*
+              * 
+              " */ /* */ /*
+              *
+              */, "/* world */"
+            end program p
+            "#
+        );
+        let line_index = LineIndex::from_source_text(code);
+        let source_code = SourceCode::new(code, &line_index);
+        let lines = LogicalLines::from_source_code(&source_code)
+            .into_iter()
+            .collect::<Vec<_>>();
+        assert_eq!(lines.len(), 3);
+        assert_eq!(lines[0].text(), "program p\n");
+        assert_eq!(
+            lines[1].text(),
+            "  print *, /**/ \"/* hello /*\" /*  \" */ /* */ /*  */, \"/* world */\"\n"
+        );
+        assert_eq!(lines[2].text(), "end program p");
+    }
+
+    #[test]
+    fn test_logical_lines_multiline_comments_unterminated_string() {
+        let code = dedent!(
+            r#"
+            program p
+              print *, " hello /*
+              */ world "
+            end program p
+            "#
+        );
+        let line_index = LineIndex::from_source_text(code);
+        let source_code = SourceCode::new(code, &line_index);
+        let transformed = LogicalLines::from_source_code(&source_code)
+            .into_iter()
+            .map(|line| line.text().to_string())
+            .collect::<Vec<_>>()
+            .join("");
+        assert_eq!(transformed, code);
+    }
+
+    #[test]
+    fn test_logical_lines_multiline_comments_and_escaped_newlines() {
+        // Should include a few more characters than before
+        let code = dedent!(
+            r#"
+            program p
+              print *, /**/ "/* hello /*" /*
+              *\ 
+              " */ /* */ /*
+              *\
+              */, "/* world */"
+            end program p
+            "#
+        );
+        let line_index = LineIndex::from_source_text(code);
+        let source_code = SourceCode::new(code, &line_index);
+        let lines = LogicalLines::from_source_code(&source_code)
+            .into_iter()
+            .collect::<Vec<_>>();
+        assert_eq!(lines.len(), 3);
+        assert_eq!(lines[0].text(), "program p\n");
+        assert_eq!(
+            lines[1].text(),
+            "  print *, /**/ \"/* hello /*\" /*  *  \" */ /* */ /*  *  */, \"/* world */\"\n"
+        );
         assert_eq!(lines[2].text(), "end program p");
     }
 }
