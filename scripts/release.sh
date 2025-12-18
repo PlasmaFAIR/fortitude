860b5fb51654329702ebaa27b56584b2e9f9e8c6
Liam Pattinson <liampattinson@gmail.com>
2025-12-18 17:58:20 +0000

    Prepare for v0.8.0rc1

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,21 +4,14 @@
 # All additional options are passed to `rooster release`
 set -eu
 
-export UV_PREVIEW=1
-
 script_root="$(realpath "$(dirname "$0")")"
 project_root="$(dirname "$script_root")"
 
 echo "Updating metadata with rooster..."
 cd "$project_root"
-uvx --from 'rooster-blue>=0.0.7' --python 3.12 --isolated -- \
-    rooster release "$@"
+
+# Update the changelog
+uvx --python 3.12 rooster@0.1.1 release "$@"
 
 echo "Updating lockfile..."
 cargo update -p fortitude
-
-echo "Generating contributors list..."
-echo ""
-echo ""
-uvx --from 'rooster-blue>=0.0.7' --python 3.12 --isolated -- \
-    rooster contributors --quiet
