98e4bc40bfa35aab328af8e056eb788e9550ca5a
Peter Hill <peter.hill@york.ac.uk>
2026-02-13 18:48:31 +0000

    Add fix for arithmetic `if`
    This turns out to be quite complicated!

--- crates/fortitude_linter/resources/test/fixtures/obsolescent/OB081.f90
+++ crates/fortitude_linter/resources/test/fixtures/obsolescent/OB081.f90
@@ -1,20 +1,152 @@
 PROGRAM test
     implicit none
-    integer x(1)
-    x(1) = 0.0
-    IF(x(1)) 30, 20, 10
-10  PRINT *, 'first case'
-    goto 40
-20  PRINT *, 'second case'
-    goto 40
-30  PRINT *, 'third case'
-40  CONTINUE
+    integer :: x(1), k, i
 
-    if (x(1) < 0) then
-      print*, "third case"
-    else if (x(1) > 0) then
-      print*, "first case"
-    else
-      print*, "second case"
-    end if
-END PROGRAM test
+    ! Permutations on three blocks
+    if (k) 1, 2, 3
+1   continue
+    print*, "<"
+    i = 0
+    goto 4
+2   continue
+    print*, "=="
+    i = 1
+    goto 4
+3   continue
+    print*, ">"
+    i = 2
+4   continue
+
+    if (k) 13, 12, 11
+11  continue
+    print*, ">"
+    i = 0
+    goto 14
+12  continue
+    print*, "=="
+    i = 1
+    goto 14
+13  continue
+    print*, "<"
+    i = 2
+14  continue
+
+    if (k) 23, 21, 22
+21  continue
+    print*, "=="
+    i = 0
+    goto 24
+22  continue
+    print*, ">"
+    i = 1
+    goto 24
+23  continue
+    print*, "<"
+    i = 2
+24  continue
+
+    ! Permutations on two blocks
+    if (k) 31, 31, 32
+31  continue
+    print*, "<="
+    i = 0
+    goto 34
+32  continue
+    print*, ">"
+    i = 2
+34  continue
+
+    if (k) 41, 42, 42
+41  continue
+    print*, "<"
+    i = 0
+    goto 44
+42  continue
+    print*, ">="
+    i = 2
+44  continue
+
+    if (k) 51, 52, 54
+51  continue
+    print*, "<"
+    i = 0
+    goto 54
+52  continue
+    print*, "=="
+    i = 2
+54  continue
+
+    ! Single block
+    if (k) 62, 61, 62
+61  print*, "=="
+62  continue
+
+    if (k) 71, 72, 71
+71  continue
+    print*, "/="
+72  continue
+
+85  do 110 k=1,9
+87     if(i-1) 100,90,100
+90     print*, "=="
+       goto 110
+100    continue
+110 continue
+
+   ! This should become:
+    do 210 k=1,9
+       if (i-1 == 0) then
+         print*, "=="
+         ! retain goto because this should reall be `cycle`
+         goto 210
+       end if
+210 continue
+
+
+contains
+  ! These should be fixable, but we have to not consume the returns
+  subroutine blocks_end_in_control_flows(M)
+    integer, intent(in) :: M
+    if (k) 1, 2, 3
+1   continue
+    print*, "<"
+    i = 0
+    return
+2   continue
+    print*, "=="
+    i = 1
+    return
+3   continue
+    print*, ">"
+    i = 2
+    continue
+  end subroutine blocks_end_in_control_flows
+
+  subroutine bar(n)
+    integer, intent(inout) :: n
+    ! TEST LESS THAN TWO POINTS
+    if(n-1)  90,90,80
+80  continue
+    ! infinite loop, but never mind that
+30  n = 1
+    goto 30
+90  return
+  end subroutine bar
+
+  subroutine unfixable_versions(x)
+    integer, intent(inout) :: x
+    if (x <= 0.0) goto 160
+    x = 0
+    ! Probably can't auto-fix because first block falls-through into second
+    if (x) 130,170,140
+130 continue
+    x = 1
+140 continue
+    x = 2
+    goto 170
+160 continue
+    x = 3
+170 continue
+  end subroutine unfixable_versions
+
+end program test
--- docs/rules/arithmetic-if.md
+++ docs/rules/arithmetic-if.md
@@ -1,4 +1,6 @@
 # arithmetic-if (OB081)
+Fix is sometimes available.
+
 This rule is unstable and in [preview](../preview.md). The `--preview` flag is required for use.
 
 This rule is turned on by default.
