a456b7ea2befaa6e9f4ceb24bf970b5da8a7c492
Peter Hill <peter.hill@york.ac.uk>
2026-04-09 12:51:44 +0100

    DO NOT MERGE: show line endings in problem test

--- crates/fortitude_linter/src/rules/style/mod.rs
+++ crates/fortitude_linter/src/rules/style/mod.rs
@@ -32,30 +32,41 @@ mod tests {
 
     use super::strings::settings::Quote;
 
-    #[test_case(Rule::LineTooLong, Path::new("S001.f90"))]
-    #[test_case(Rule::MissingNewlineAtEndOfFile, Path::new("S002.f90"))]
-    #[test_case(Rule::UnnamedEndStatement, Path::new("S061.f90"))]
-    #[test_case(Rule::MissingDoubleColon, Path::new("S071.f90"))]
-    #[test_case(Rule::SuperfluousSemicolon, Path::new("S081.f90"))]
-    #[test_case(Rule::MultipleStatementsPerLine, Path::new("S082.f90"))]
-    #[test_case(Rule::TrailingWhitespace, Path::new("S101.f90"))]
-    #[test_case(Rule::IncorrectSpaceBeforeComment, Path::new("S102.f90"))]
-    #[test_case(Rule::IncorrectSpaceAroundDoubleColon, Path::new("S103.f90"))]
-    #[test_case(Rule::IncorrectSpaceBetweenBrackets, Path::new("S104.f90"))]
-    #[test_case(Rule::SuperfluousImplicitNone, Path::new("S201.f90"))]
-    #[test_case(Rule::MultipleModules, Path::new("S211.f90"))]
-    #[test_case(Rule::ProgramWithModule, Path::new("S212.f90"))]
-    #[test_case(Rule::FunctionMissingResult, Path::new("S221.f90"))]
-    #[test_case(Rule::KeywordsMissingSpace, Path::new("S231.f90"))]
-    #[test_case(Rule::KeywordHasWhitespace, Path::new("S231.f90"))]
-    #[test_case(Rule::BadQuoteString, Path::new("S241.f90"))]
-    #[test_case(Rule::AvoidableEscapedQuote, Path::new("S242.f90"))]
-    #[test_case(Rule::UselessReturn, Path::new("S251.f90"))]
+    use std::borrow::Cow;
+
+    pub(crate) trait ShowNonprinting {
+        fn show_nonprinting(&self) -> Cow<'_, str>;
+    }
+
+    macro_rules! impl_show_nonprinting {
+        ($(($from:expr, $to:expr)),+) => {
+            impl ShowNonprinting for str {
+                fn show_nonprinting(&self) -> Cow<'_, str> {
+                    if self.find(&[$($from),*][..]).is_some() {
+                        Cow::Owned(
+                            self.$(replace($from, $to)).*
+                        )
+                    } else {
+                        Cow::Borrowed(self)
+                    }
+                }
+            }
+        };
+    }
+
+    impl_show_nonprinting!(
+        ('\x07', "␇"),
+        ('\x08', "␈"),
+        ('\x1b', "␛"),
+        ('\x7f', "␡"),
+        ('\x0A', "␊\n"),
+        ('\x0D', "␍")
+    );
+
     #[test_case(Rule::SuperfluousElseReturn, Path::new("S252.f90"))]
     #[test_case(Rule::SuperfluousElseCycle, Path::new("S253.f90"))]
     #[test_case(Rule::SuperfluousElseExit, Path::new("S254.f90"))]
     #[test_case(Rule::SuperfluousElseStop, Path::new("S255.f90"))]
-    #[test_case(Rule::UnsortedUses, Path::new("S271.f90"))]
     fn rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!("{}_{}", rule_code.as_ref(), path.to_string_lossy());
         let diagnostics = test_path(
@@ -63,196 +74,7 @@ mod tests {
             &CheckSettings::for_rule(rule_code),
         )?;
         apply_common_filters!();
-        assert_snapshot!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Rule::SuperfluousImplicitNone, Path::new("S201_ok.f90"))]
-    #[test_case(Rule::UnsortedUses, Path::new("S271_ok.f90"))]
-    fn rules_pass(rule_code: Rule, path: &Path) -> Result<()> {
-        let diagnostics = test_path(
-            Path::new("style").join(path).as_path(),
-            &CheckSettings::for_rule(rule_code),
-        )?;
-        assert!(
-            diagnostics.is_empty(),
-            "Test source has no warnings, but some were raised:\n{diagnostics}"
-        );
-        Ok(())
-    }
-
-    #[test_case(Rule::LineTooLong, Path::new("S001_line_length_20.f90"))]
-    fn line_too_long_line_length_20(rule_code: Rule, path: &Path) -> Result<()> {
-        let snapshot = format!("{}_{}", rule_code.as_ref(), path.to_string_lossy());
-
-        let settings = CheckSettings {
-            line_length: 20,
-            ..CheckSettings::for_rule(rule_code)
-        };
-        let diagnostics = test_path(Path::new("style").join(path).as_path(), &settings)?;
-        apply_common_filters!();
-        assert_snapshot!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Rule::LineTooLong, Path::new("S001_ignore_comment_length.f90"))]
-    fn ignore_comment_length(rule_code: Rule, path: &Path) -> Result<()> {
-        let snapshot = format!("{}_{}", rule_code.as_ref(), path.to_string_lossy());
-
-        let settings = CheckSettings {
-            line_too_long: line_length::settings::Settings {
-                ignore_comments: true,
-            },
-            ..CheckSettings::for_rule(rule_code)
-        };
-        let diagnostics = test_path(Path::new("style").join(path).as_path(), &settings)?;
-        apply_common_filters!();
-        assert_snapshot!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Rule::KeywordsMissingSpace, Path::new("S231.f90"))]
-    #[test_case(Rule::KeywordHasWhitespace, Path::new("S231.f90"))]
-    fn keyword_whitespace_include_inout_goto(rule_code: Rule, path: &Path) -> Result<()> {
-        let snapshot = format!(
-            "{}_{}_include_inout_goto",
-            rule_code.as_ref(),
-            path.to_string_lossy()
-        );
-
-        #[allow(clippy::needless_update)]
-        let settings = CheckSettings {
-            keyword_whitespace: keywords::settings::Settings {
-                inout_with_space: true,
-                goto_with_space: true,
-            },
-            ..CheckSettings::for_rule(rule_code)
-        };
-        let diagnostics = test_path(Path::new("style").join(path).as_path(), &settings)?;
-        apply_common_filters!();
-        assert_snapshot!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Rule::BadQuoteString, Path::new("S241.f90"))]
-    fn bad_quote_string_single_quotes(rule_code: Rule, path: &Path) -> Result<()> {
-        let snapshot = format!(
-            "{}_{}_include_inout_goto",
-            rule_code.as_ref(),
-            path.to_string_lossy()
-        );
-
-        let settings = CheckSettings {
-            strings: strings::settings::Settings {
-                quotes: Quote::Single,
-            },
-            ..CheckSettings::for_rule(rule_code)
-        };
-        let diagnostics = test_path(Path::new("style").join(path).as_path(), &settings)?;
-        apply_common_filters!();
-        assert_snapshot!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(
-        Rule::InconsistentArrayDeclaration,
-        Path::new("S261.f90"),
-        PreferAttribute::Always
-    )]
-    #[test_case(
-        Rule::InconsistentArrayDeclaration,
-        Path::new("S261.f90"),
-        PreferAttribute::Never
-    )]
-    #[test_case(
-        Rule::InconsistentArrayDeclaration,
-        Path::new("S261.f90"),
-        PreferAttribute::Keep
-    )]
-    #[test_case(
-        Rule::MixedScalarArrayDeclaration,
-        Path::new("S262.f90"),
-        PreferAttribute::Always
-    )]
-    #[test_case(
-        Rule::MixedScalarArrayDeclaration,
-        Path::new("S262.f90"),
-        PreferAttribute::Never
-    )]
-    #[test_case(
-        Rule::MixedScalarArrayDeclaration,
-        Path::new("S262.f90"),
-        PreferAttribute::Keep
-    )]
-    #[test_case(
-        Rule::BadArrayDeclaration,
-        Path::new("S263.f90"),
-        PreferAttribute::Always
-    )]
-    #[test_case(
-        Rule::BadArrayDeclaration,
-        Path::new("S263.f90"),
-        PreferAttribute::Never
-    )]
-    // This one should be a no-op
-    #[test_case(
-        Rule::BadArrayDeclaration,
-        Path::new("S263.f90"),
-        PreferAttribute::Keep
-    )]
-    fn inconsistent_dimensions(
-        rule_code: Rule,
-        path: &Path,
-        prefer_attribute: PreferAttribute,
-    ) -> Result<()> {
-        let snapshot = format!(
-            "{}_{}_{}",
-            rule_code.as_ref(),
-            path.to_string_lossy(),
-            prefer_attribute
-        );
-
-        let settings = CheckSettings {
-            inconsistent_dimension: inconsistent_dimension::settings::Settings { prefer_attribute },
-            ..CheckSettings::for_rule(rule_code)
-        };
-        let diagnostics = test_path(Path::new("style").join(path).as_path(), &settings)?;
-        apply_common_filters!();
-        assert_snapshot!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Rule::TooComplex, Path::new("S901.f90"))]
-    fn too_complex_threshold_5(rule_code: Rule, path: &Path) -> Result<()> {
-        let snapshot = format!("{}_{}", rule_code.as_ref(), path.to_string_lossy());
-
-        let settings = CheckSettings {
-            complexity: complexity::settings::Settings {
-                max_complexity: 5,
-                ..Default::default()
-            },
-            ..CheckSettings::for_rule(rule_code)
-        };
-        let diagnostics = test_path(Path::new("style").join(path).as_path(), &settings)?;
-        apply_common_filters!();
-        assert_snapshot!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Rule::TooManyArguments, Path::new("S902.f90"))]
-    fn too_many_arguments_threshold_5(rule_code: Rule, path: &Path) -> Result<()> {
-        let snapshot = format!("{}_{}", rule_code.as_ref(), path.to_string_lossy());
-
-        let settings = CheckSettings {
-            complexity: complexity::settings::Settings {
-                max_args: 4,
-                ..Default::default()
-            },
-            ..CheckSettings::for_rule(rule_code)
-        };
-        let diagnostics = test_path(Path::new("style").join(path).as_path(), &settings)?;
-        apply_common_filters!();
-        assert_snapshot!(snapshot, diagnostics);
+        assert_snapshot!(snapshot, diagnostics.show_nonprinting());
         Ok(())
     }
 }
