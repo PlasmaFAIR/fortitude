70122a9fea8bb4058f2222036cede2a29f8cf9ea
connoraird <c.aird@ucl.ac.uk>
2026-06-24 17:34:22 +0100

    Test new implementation with rule S106

--- crates/fortitude_linter/src/rules/style/mod.rs
+++ crates/fortitude_linter/src/rules/style/mod.rs
@@ -47,6 +47,7 @@ mod tests {
     #[test_case(Rule::IncorrectSpaceAroundDoubleColon, Path::new("S103.f90"))]
     #[test_case(Rule::IncorrectSpaceBetweenBrackets, Path::new("S104.f90"))]
     #[test_case(Rule::InvalidIndentationMultiple, Path::new("S105.f90"))]
+    #[test_case(Rule::InvalidPreprocIndentation, Path::new("S106.f90"))]
     #[test_case(Rule::SuperfluousImplicitNone, Path::new("S201.f90"))]
     #[test_case(Rule::MultipleModules, Path::new("S211.f90"))]
     #[test_case(Rule::ProgramWithModule, Path::new("S212.f90"))]
@@ -122,7 +123,7 @@ mod tests {
     }
 
     #[test]
-    fn incorrect_indent_width_2() -> Result<()> {
+    fn invalid_indentation_multiple_width_2() -> Result<()> {
         let rule_code = Rule::InvalidIndentationMultiple;
         let path = Path::new("S105.f90");
         let snapshot = format!(
